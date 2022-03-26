4d30b0165c1bb7f50d37a1bf7d48a003c7e191f0
William Skillas <wmskillas@gmail.com>
2022-03-25 23:02:08 -0400

    moved files around and did some more javascript, relinked css and js

--- 02-Homework/Develop/java.js
+++ 02-Homework/Develop/java.js
@@ -6,6 +6,7 @@ var chooseLength;
 
 
 var characters = ["!", "@", "#", "$", "%", " ^ ", "&", "*","(", ")", "+", ",", "-", ".", "/", ":", ";", "'", " < ", "=", " > ", " ? ", "[", "]", "_", "", "|", "}", "~"];
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -13,7 +14,6 @@ console.log(characters);
 console.log(numbers);
 console.log(upperCase);
 console.log(lowerCase);
-
 function generatePassword(){
   chooseLength = prompt("Please choose 8 to 128 Characters for your Passsword!");
   if (chooseLength <= 7 || chooseLength >=127){
@@ -23,13 +23,16 @@ function generatePassword(){
     chooseNumber = confirm("Do you want numbers?");
     chooseCharacter = confirm("Do you want special characters?");
     chooseUppercase = confirm("Do you want Uppercase letters?");
-    chooseLowerCase = confirm("Do you want Lowercase letters?");
+    chooseLowercase = confirm("Do you want Lowercase letters?");
   };
+
   if (!chooseNumber && !chooseCharacter && !chooseUpperCase && !chooseLowerCase){
+    choose = alert("Well what would you like this made of then cause thats all I got")
     choose = alert("Well what would you like this made of then, cause thats all I got");
   }
-  else if(chooseNumber && chooseCharacter && chooseLowerCase && chooseUpperCase){
-    choose = numbers.concat(characters, lowerCase, upperCase);
+  else if(chooseNumber && chooseCharacter && chooseLowercase && chooseUpperCase){
+
+  choose = characters.concat(numbers, upperCase, lowerCase);
   }
   else if(chooseNumber && chooseCharacter && chooseUpperCase){
     choose = numbers.concat(characters, upperCase);
@@ -37,10 +40,10 @@ function generatePassword(){
   else if(chooseNumber && chooseCharacter && chooseLowerCase){
     choose = numbers.concat(characters, lowerCase);
   }
-  else if(chooseNumber && chooseUpperCase && chooseLowerCase){
+  else if(chooseNumber && chooseUpperCase && chooseLowercase){
     choose = numbers.concat(upperCase, lowerCase);
   }
-  else if(chooseCharacter && chooseUpperCase && chooseLowerCase){
+  else if(chooseCharacter && chooseUpperCase && chooseLowercase){
     choose = characters.concat(upperCase, lowerCase);
   }
   else if(chooseNumber && chooseCharacter){
@@ -76,32 +79,23 @@ function generatePassword(){
 
 var password = [];
 for (i = 0; i < chooseLength; i++){
-  var pickChoices = choose[Math.floor(Math.random() * chooseLength.length)];
+  var pickChoices = choose[Math.floor(Math.random() * choose.length)];
   password.push(pickChoices);
 }
-console.log(password)
+console.log(password);
   //write the logic of you code in here
   //whatever value is returned from this function with then be stored in the password variable
   // password += // look up +=
 }
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+  
 }
-function renderLastRegistered(){
-  var password = localStorage.getItem("password");
-  localStorage.setItem("password", password)
-  renderLastRegistered();
-}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
