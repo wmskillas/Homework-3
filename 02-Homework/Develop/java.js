88edcaf3e142e7b2aaa8bfa9d24c00ee94f0f076
William Skillas <wmskillas@gmail.com>
2022-03-25 23:23:42 -0400

    made a new javafile and moved files around, this is hard i can get password to show on console but can't get it to show in the box'

--- 02-Homework/Develop/java.js
+++ 02-Homework/Develop/java.js
@@ -6,7 +6,6 @@ var chooseLength;
 
 
 var characters = ["!", "@", "#", "$", "%", " ^ ", "&", "*","(", ")", "+", ",", "-", ".", "/", ":", ";", "'", " < ", "=", " > ", " ? ", "[", "]", "_", "", "|", "}", "~"];
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -14,6 +13,9 @@ console.log(characters);
 console.log(numbers);
 console.log(upperCase);
 console.log(lowerCase);
+
+var choose;
+
 function generatePassword(){
   chooseLength = prompt("Please choose 8 to 128 Characters for your Passsword!");
   if (chooseLength <= 7 || chooseLength >=127){
@@ -94,7 +96,6 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-  
 }
 
 // Add event listener to generate button
